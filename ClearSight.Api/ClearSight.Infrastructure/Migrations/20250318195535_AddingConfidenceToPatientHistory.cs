8156724ba2bec66cf361c22abb293c49f03c37a2
Mina <minafarag0001@gmail.com>
2025-05-24 00:27:54 +0300

    MigrateForNewFileds

---
+++ ClearSight.Api/ClearSight.Infrastructure/Migrations/20250318195535_AddingConfidenceToPatientHistory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClearSight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingConfidenceToPatientHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Confidence",
+                table: "PatientHistories",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Confidence",
+                table: "PatientHistories");
+        }
+    }
+}
---
+++ ClearSight.Api/ClearSight.Infrastructure/Migrations/20250523215520_AddingDoctorVerification.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClearSight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDoctorVerification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Doctors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UploadedDocumentPath",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    AdminId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.AdminId);
+                    table.ForeignKey(
+                        name: "FK_Admins_Users_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "UploadedDocumentPath",
+                table: "Doctors");
+        }
+    }
+}
